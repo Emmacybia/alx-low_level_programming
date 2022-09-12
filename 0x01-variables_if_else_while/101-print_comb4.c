f45960d8ad5ed12ced72f0568955d8b316dd6f7a
Emmacybia <emma.cybia7531@gmail.com>
2022-09-11 20:12:01 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: using the main function
+ * this program prints "All possible different combinations of 3 digits"
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c = 0;
+	int b;
+	int d;
+	int f;
+	
+	while (c <= 999)
+	{
+		b = (c / 100 + '0');
+		d = (c / 10 % 10 + '0');
+		f = (c % 10 + '0');
+		if ((b < d) && (d < c))
+		{
+			putchar(b);
+			putchar(d);
+			putchar(f);
+
+			if (c != 789)
+			{
+				putchar(',');
+				putchar(' ');
+		}
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
