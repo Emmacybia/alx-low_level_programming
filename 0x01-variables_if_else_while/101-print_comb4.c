afafbee564893c57f981ffed2832b95344b1c80d
Emmacybia <emma.cybia7531@gmail.com>
2022-09-12 13:56:25 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,14 +12,13 @@ int main(void)
 	int c = 0;
 	int b;
 	int d;
-	int f;
-	
+	int f;	
 	while (c <= 999)
 	{
 		b = (c / 100 + '0');
 		d = (c / 10 % 10 + '0');
 		f = (c % 10 + '0');
-		if ((b < d) && (d < c))
+		if ((b < d) && (d < f))
 		{
 			putchar(b);
 			putchar(d);
